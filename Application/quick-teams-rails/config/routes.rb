a7a253bc79fc97e6b25525aba9687410d5b57460
Chris <c.francis7@live.ca>
2019-11-20 22:36:36 -0500

    added new fields on sign up

--- Application/quick-teams-rails/config/routes.rb
+++ Application/quick-teams-rails/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   
   root to: 'pages#home'
 
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
 
   resource :teams 
   get '/teams/:id/message_board', to: 'teams#message_board'
