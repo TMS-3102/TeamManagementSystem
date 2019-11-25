d6b1aac5dbd498f1b2cc07f4634f3be80693345c
SamWorrod <samworrod@gmail.com>
2019-11-25 09:48:53 -0500

    Commit

--- Application/quick-teams-rails/app/controllers/accounts_controller.rb
+++ Application/quick-teams-rails/app/controllers/accounts_controller.rb
@@ -3,4 +3,11 @@ class AccountsController < ApplicationController
     def show
         @user=User.find(params[:id])
     end
+
+    def course
+        @course=Course.new
+        course.name=params[:coursename]
+        course.code=params[:coursecode]
+        course.save
+    end
 end
