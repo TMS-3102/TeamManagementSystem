c7283a50ec72e2790369f68861c26db6f98c73b9
Jaimin Patel <jaiminpatel98@gmail.com>
2020-11-17 17:53:38 -0500

    tasks-users table creation

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,4 @@
 class Task < ApplicationRecord
     belongs_to :team
+    has_and_belongs_to_many :users
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201117220845) do
+ActiveRecord::Schema.define(version: 20201117224309) do
 
   create_table "courses", force: :cascade do |t|
     t.datetime "created_at",    null: false
@@ -62,6 +62,13 @@ ActiveRecord::Schema.define(version: 20201117220845) do
     t.integer  "team_id"
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
+  end
+
+  create_table "tasks_users", force: :cascade do |t|
+    t.integer  "task_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "teams", force: :cascade do |t|
